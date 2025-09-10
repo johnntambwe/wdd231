4c20a9ba326ece131950f210ccda696ed9ec7be2
John Ntambwe <johnntambwe02@gmail.com>
2025-09-10 16:33:29 -0700

    week1 home page completed

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,6 +1,20 @@
+
 let currentYear = document.querySelector(".jsCurrentYear");
 let lastModified = document.querySelector("#lastModified");
 const date = new Date();
- 
+
 currentYear.innerHTML = date.getFullYear();
 lastModified.innerHTML = document.lastModified;
+
+// humbuger menu
+// Store the selected elements that we are going to use. This is not required but a good practice with larger programs where the variable will be referenced more than once.
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+// --------------------------------------
